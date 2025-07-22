9421d9450979c8a0b2d8b2c4b049bfc1f98cd729
Justin Lee <justin.adam.lee@gmail.com>
2025-07-21 21:08:23 -0400

    change order

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -146,10 +146,10 @@
   services.tlp = {
     enable = true;
     settings = {
-      CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_BOOST_ON_AC = 1;
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
     };
   };
 
