199b93f14e36664c40034a314ce495854733ef65
Justin Lee <justin.adam.lee@gmail.com>
2025-07-21 21:15:22 -0400

    more settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -146,10 +146,12 @@
   services.tlp = {
     enable = true;
     settings = {
-      CPU_BOOST_ON_BAT = 0;
-      CPU_BOOST_ON_AC = 1;
+      PLATFORM_PROFILE_ON_BAT = "powersave";
+      PLATFORM_PROFILE_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_BOOST_ON_BAT = 0;
+      CPU_BOOST_ON_AC = 1;
     };
   };
 
