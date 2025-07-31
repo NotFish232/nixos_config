6e65737de3ff25ce4b8394a82c58d7e3fc8fe7fe
Justin Lee <justin.adam.lee@gmail.com>
2025-07-31 17:40:07 -0400

    add funnel

--- not-server/configuration.nix
+++ not-server/configuration.nix
@@ -127,4 +127,16 @@
     };
   };
 
+
+  systemd.services.funnel-github-readme-stats = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    description = "Tailscale forward the github-readme-stats repository";
+    serviceConfig = {
+      Type = "simple";
+      User = "justin";
+      ExecStart = ''${pkgs.tailscale}/bin/tailscale funnel 9000'';
+    };
+  };
+
 }
