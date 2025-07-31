bf9241f0048e0fb0f37edd46e5bfd0ce94328be6
Justin Lee <justin.adam.lee@gmail.com>
2025-07-31 17:36:49 -0400

    switch type

--- not-server/configuration.nix
+++ not-server/configuration.nix
@@ -115,11 +115,12 @@
   system.stateVersion = "25.05"; # Did you read the comment?
 
   systemd.services.github-readme-stats = {
+    path = with pkgs; [ git ];
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     description = "Run the github-readme-stats repository";
     serviceConfig = {
-      Type = "notify";
+      Type = "simple";
       User = "justin";
       WorkingDirectory = "/home/justin/github-readme-stats";
       ExecStart = ''${pkgs.nix}/bin/nix develop --command ${pkgs.nodejs_24}/bin/node express.js'';
