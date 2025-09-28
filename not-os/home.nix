bf34bc2fb69f4d8cbf62aca052f8906ad91000d0
Justin Lee <justin.adam.lee@gmail.com>
2025-09-28 18:30:46 -0400

    fix rust

--- not-os/home.nix
+++ not-os/home.nix
@@ -112,7 +112,10 @@
     })
   ];
 
-  home.sessionVariables.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  home.sessionVariables = {
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH";
+    LD_LIBRARY_PATH = "${pkgs.openssl.out}/lib:$LD_LIBRARY_PATH";
+  };
 
   # direnv and nix-direnv
   programs.direnv.enable = true;
