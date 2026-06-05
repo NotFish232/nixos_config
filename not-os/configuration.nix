427f24e35293b43a24647d773af9c794c64381da
NotFish232 <justin.adam.lee@gmail.com>
2026-06-05 14:26:18 -0400

    switch to ppd

--- not-os/configuration.nix
+++ not-os/configuration.nix
@@ -155,17 +155,17 @@
   services.tailscale.enable = true;
 
   # Switch to TLP for power profiles
-  services.power-profiles-daemon.enable = false;
+  services.power-profiles-daemon.enable = true;
   # services.auto-cpufreq.enable = true;
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_BOOST_ON_BAT = 0;
-      CPU_BOOST_ON_AC = 1;
-    };
-  };
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
+  #     CPU_BOOST_ON_BAT = 0;
+  #     CPU_BOOST_ON_AC = 1;
+  #   };
+  # };
 
   # Keyd and map copilot btn to ctrl
   services.keyd = {
